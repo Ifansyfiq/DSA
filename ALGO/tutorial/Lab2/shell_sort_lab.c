1226c805a3698576677e21b5fa243f98df23ac11
Hayai <108877345+Ifansyfiq@users.noreply.github.com>
2022-11-08 15:29:30 +0800

    update shell sort

--- ALGO/tutorial/Lab2/shell_sort_lab.c
+++ ALGO/tutorial/Lab2/shell_sort_lab.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <time.h>
 
-void shell_sort(long [], long);
+int shell_sort(long [], long);
 void display(long [], long);
 clock_t start, end;
 double cpu_time_used;
@@ -11,20 +11,24 @@ void main()
 {
     int num=1, n = 5000;
     long array[30000];
+    float elapseMsec;
 
     for(int i = 1; i <= 6; i++)
     {   srand(time(0));
         tot_comp=0,tot_step=0;
         for(int j=0; j<n; j++)
             array[j]=rand()%n;
-        start = clock();
+        //start = clock();
+        start = clock() * CLK_TCK;
         shell_sort(array, n);
-        end = clock();
-        cpu_time_used = (double)(end-start)/CLOCKS_PER_SEC;
+       end = clock() * CLK_TCK;
+        elapseMsec = end / 1000;
+        cpu_time_used = elapseMsec / 1000;
+        /*end = clock();
+        cpu_time_used = (double)(end-start)/CLOCKS_PER_SEC;*/
         printf("\n\nSECTION : %d", i);
         printf("\nSIZE : %d", n);
         printf("\nTIME: %.4f seconds", cpu_time_used);
-//            printf("\n%.4f", cpu_time_used);
         printf("\nTOTAL COMPARE: %d", tot_comp);
         printf("\nTOTAL STEP: %d", tot_step);
         printf("\n============================");
@@ -32,20 +36,25 @@ void main()
     }
 }
 
-void shell_sort(long heap[], long no)
+int shell_sort(long shell[], long no)
 {
  for (int interval = no/2; interval > 0; interval /= 2)
     {
         for (int i = interval; i < no; i += 1)
         {
             /* store a[i] to the variable temp and make the ith position empty */
-            int temp = heap[i];
+            int temp = shell[i];
             int j;
-            for (j = i; j >= interval && heap[j - interval] > temp; j -= interval)
-                heap[j] = heap[j - interval];
-
+            for (j = i; j >= interval && shell[j - interval] > temp; j -= interval)
+            {
+               shell[j] = shell[j - interval];
+                tot_comp++;
+            }
             // put temp (the original a[i]) in its correct position
-            heap[j] = temp;
+            shell[j] = temp;
+            tot_step++;
         }
+
     }
+    return 0;
 }
